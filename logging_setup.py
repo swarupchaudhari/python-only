99c3df134065393085d6c661c5e6b1c4e575a2fc
swarupchaudhari <chaudhariswarup266@gmail.com>
2026-01-05 08:22:31 +0530

    Update logging_setup.py

--- logging_setup.py
+++ logging_setup.py
@@ -1,17 +1,15 @@
 import logging
 
-def setup_logger(name):
+
+def setup_logger(name, log_file='server.log', level=logging.DEBUG):
+    # Create a custom logger
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
 
-    if not logger.handlers:
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(formatter)
-
-        logger.addHandler(console_handler)
+    # Configure the custom logger
+    logger.setLevel(level)
+    file_handler = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
     return logger
